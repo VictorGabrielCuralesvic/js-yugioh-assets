a667ec580d5da1938b768fff0b9e415f83a08bb4
Nome <miranhaomi@gmail.com>
2023-11-28 18:12:46 -0300

    refatorando o código
    deixando a função setCardsField somente para chamar outras funlções

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -97,17 +97,45 @@ async function setCardsField(cardId) {
 
     let computerCardId = await getRandomCardId();
 
-    state.fieldCards.player.style.display = "block";
-    state.fieldCards.computer.style.display = "block";
+    await showHiddenCardFieldsImages(true);
 
-    state.fieldCards.player.src = cardData[cardId].img;
-    state.fieldCards.computer.src = cardData[computerCardId].img;
+    await hiddenCardDetails();
+
+    await drawCardsInField(cardId, computerCardId);
 
     let duelResults = await checkDuelResults(cardId, computerCardId);
 
     await updateScore();
     await drawButton(duelResults);
 }
+
+async function drawCardsInField(cardId, computerCardId) {
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+}
+
+async function hiddenCardDetails() {
+
+    state.cardSprites.name.innerText = "";
+    state.cardSprites.type.innerText = "";
+    state.cardSprites.avatar.src = "";
+
+};
+
+async function showHiddenCardFieldsImages(value) {
+
+    if(value === true) {
+        state.fieldCards.player.style.display = "block";
+        state.fieldCards.computer.style.display = "block";
+    }
+
+    if (value === false) {
+        state.fieldCards.player.style.display = "none";
+        state.fieldCards.computer.style.display = "none";
+    }
+};
 
 async function checkDuelResults(playerCardId, computerCardId) {
 
@@ -181,6 +209,9 @@ async function drawCards(cardNumbers, fieldSide) {
 };
 
 function init() {
+
+    showHiddenCardFieldsImages(false);
+
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
 };
