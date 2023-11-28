04a5f33d79d589fc65b9e4af0f3827c2915b5beb
Nome <miranhaomi@gmail.com>
2023-11-28 15:55:14 -0300

    Assinatura das funções principais

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,6 +19,11 @@ const state = {
     }
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
+
 const pathImages = "src/assets/icons/";
 
 const cardData = [
@@ -42,14 +47,24 @@ const cardData = [
         id:2,
         name: "Exodia",
         type: "Scissors",
-        img: `${pathImages}dragon.png`,
+        img: `${pathImages}exodia.png`,
         WinOf: [0],
         LoseOf: [1],
     },
 ];
 
-function init() {
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
 
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 };
 
 init();
